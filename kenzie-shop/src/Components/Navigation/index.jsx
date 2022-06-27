34a387721bea0c9470760cf20e5c1954d5ce591b
Matheus <matheus_lsilva@hotmail.com>
2022-06-26 21:53:44 -0300

    <fix> removendo consoles e comentarios

--- kenzie-shop/src/Components/Navigation/index.jsx
+++ kenzie-shop/src/Components/Navigation/index.jsx
@@ -3,80 +3,80 @@ import React, { useState } from 'react';
 import NavigationContainer from './style';
 import StyledButton from '../Button/style';
 
-import { FiSearch } from 'react-icons/fi'
+import { FiSearch } from 'react-icons/fi';
 import { useDispatch } from 'react-redux';
-import { allProductsFilterThunk, bakeryFilterThunk, dairyFilterThunk, hortifruitFilterThunk, inputFilterThunk } from '../../store/modules/products/thunk';
+import {
+  allProductsFilterThunk,
+  bakeryFilterThunk,
+  dairyFilterThunk,
+  hortifruitFilterThunk,
+  inputFilterThunk
+} from '../../store/modules/products/thunk';
 
 const Navigation = () => {
-  
-  const [teste, setTeste] = useState('')
-  
-  const DISPATCH = useDispatch() 
- 
-  const handleFilter = (section) =>{
-    switch(section){
-      
+  const [teste, setTeste] = useState('');
+
+  const DISPATCH = useDispatch();
+
+  const handleFilter = section => {
+    switch (section) {
       case 'Hortifruit':
-         return DISPATCH(hortifruitFilterThunk(section))
-         
+        return DISPATCH(hortifruitFilterThunk(section));
+
       case 'Panificadora':
-         return DISPATCH(bakeryFilterThunk(section))
-      
+        return DISPATCH(bakeryFilterThunk(section));
+
       case 'Laticinios':
-        return DISPATCH(dairyFilterThunk(section))
+        return DISPATCH(dairyFilterThunk(section));
 
       default:
-        return DISPATCH(allProductsFilterThunk())
+        return DISPATCH(allProductsFilterThunk());
     }
-  }
+  };
 
-  const handleInputFilter = (e) =>{
-    console.log(e.target.value)
-    DISPATCH(inputFilterThunk(e.target.value))
-  }
+  const handleInputFilter = e => {
+    DISPATCH(inputFilterThunk(e.target.value));
+  };
 
   return (
-     <NavigationContainer>
+    <NavigationContainer>
       <nav>
-        <StyledButton
-         contentType='text'
-         onClick={handleFilter}
-        >
+        <StyledButton contentType="text" onClick={handleFilter}>
           Produtos
         </StyledButton>
         <StyledButton
-         contentType='text'
-        onClick={() => handleFilter('Hortifruit')}
+          contentType="text"
+          onClick={() => handleFilter('Hortifruit')}
         >
           Hortifruit
         </StyledButton>
         <StyledButton
-         contentType='text'
-         onClick={() => handleFilter('Panificadora')}
+          contentType="text"
+          onClick={() => handleFilter('Panificadora')}
         >
           Panificadora
         </StyledButton>
         <StyledButton
-         contentType='text'
-         onClick={() => handleFilter('Laticinios')}
-         >
+          contentType="text"
+          onClick={() => handleFilter('Laticinios')}
+        >
           Laticínios
         </StyledButton>
       </nav>
       <div>
-        <input 
+        <input
           type="text"
-          placeholder='Pesquisar' 
-          onChange={(e) => handleInputFilter(e)}
+          placeholder="Pesquisar"
+          onChange={e => handleInputFilter(e)}
         />
         <StyledButton
-         contentType='icon'
-        //  onClick={handleInputFilter}
-         >
-          <FiSearch size={15}/>
+          contentType="icon"
+          //  onClick={handleInputFilter}
+        >
+          <FiSearch size={15} />
         </StyledButton>
       </div>
-     </NavigationContainer> 
+    </NavigationContainer>
   );
 };
 
